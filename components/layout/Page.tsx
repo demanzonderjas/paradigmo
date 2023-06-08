43298aa95feb45122c64b2a803ffe992fb1be75a
= <=>
2023-06-08 14:12:19 +0200

    add titles

--- components/layout/Page.tsx
+++ components/layout/Page.tsx
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import { Login } from "../user/Login";
 import { Menu } from "./Menu";
 
 export function Page({ title, children }: { title: string; children?: any }) {
 	return (
 		<>
+			<Head>
+				<title>{title} - Paradigmo</title>
+			</Head>
 			<Menu />
 			<h1>{title}</h1>
 			{children}
