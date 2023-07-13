465c759dc87971398b047efa859c3753f3805ae8
Daan Weustenraad <weustenraad@gmail.com>
2023-07-13 09:27:18 +0200

    sort tags in alphabetical order

--- components/tags/TagList.tsx
+++ components/tags/TagList.tsx
@@ -9,29 +9,31 @@ export const TagList: React.FC<{ tags: TTag[]; hasLinks?: boolean; removeTag?: F
 	const { push } = useRouter();
 	return (
 		<div className="tags flex gap-5 flex-wrap w-full">
-			{tags.map((tag, index) => (
-				<div
-					className={`tag flex items-stretch justify-center ${
-						hasLinks
-							? "cursor-pointer hover:bg-black hover:text-white transition-transform hover:scale-110"
-							: ""
-					}`}
-					key={index}
-					onClick={hasLinks ? () => push(`/tags/${tag.uid}`) : undefined}
-				>
-					<span className="flex items-center justify-center py-2 px-4 bg-white text-black">
-						{tag.name}
-					</span>
-					{!!removeTag && (
-						<span
-							className="bg-orange flex items-center text-white px-2"
-							onClick={() => removeTag(tag.uid)}
-						>
-							x
+			{tags
+				.sort((a, b) => (b.name > a.name ? -1 : 1))
+				.map((tag, index) => (
+					<div
+						className={`tag flex items-stretch justify-center ${
+							hasLinks
+								? "cursor-pointer hover:bg-black hover:text-white transition-transform hover:scale-110"
+								: ""
+						}`}
+						key={index}
+						onClick={hasLinks ? () => push(`/tags/${tag.uid}`) : undefined}
+					>
+						<span className="flex items-center justify-center py-2 px-4 bg-white text-black">
+							{tag.name}
 						</span>
-					)}
-				</div>
-			))}
+						{!!removeTag && (
+							<span
+								className="bg-orange flex items-center text-white px-2"
+								onClick={() => removeTag(tag.uid)}
+							>
+								x
+							</span>
+						)}
+					</div>
+				))}
 		</div>
 	);
 };
