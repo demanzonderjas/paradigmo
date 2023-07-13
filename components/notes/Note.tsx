e5303773a61afdc325d8934549482756839b1231
Daan Weustenraad <weustenraad@gmail.com>
2023-07-13 09:21:02 +0200

    add view icon

--- components/notes/Note.tsx
+++ components/notes/Note.tsx
@@ -6,7 +6,8 @@ import { Icon } from "../layout/Icon";
 import { isLink } from "../../utils/validation";
 
 export const Note: React.FC<TNote> = (note) => {
-	const { push } = useRouter();
+	const { push, pathname } = useRouter();
+	const isView = /view/.test(pathname);
 
 	return (
 		<div
@@ -33,6 +34,14 @@ export const Note: React.FC<TNote> = (note) => {
 						fillRule="nonzero"
 					/>
 				</Icon>
+				{!isView && (
+					<Icon handleClick={() => push(`/notes/view/${note.uid}`)} evenOdd={true}>
+						<path
+							d="m11.998 5c-4.078 0-7.742 3.093-9.853 6.483-.096.159-.145.338-.145.517s.048.358.144.517c2.112 3.39 5.776 6.483 9.854 6.483 4.143 0 7.796-3.09 9.864-6.493.092-.156.138-.332.138-.507s-.046-.351-.138-.507c-2.068-3.403-5.721-6.493-9.864-6.493zm8.413 7c-1.837 2.878-4.897 5.5-8.413 5.5-3.465 0-6.532-2.632-8.404-5.5 1.871-2.868 4.939-5.5 8.404-5.5 3.518 0 6.579 2.624 8.413 5.5zm-8.411-4c2.208 0 4 1.792 4 4s-1.792 4-4 4-4-1.792-4-4 1.792-4 4-4zm0 1.5c-1.38 0-2.5 1.12-2.5 2.5s1.12 2.5 2.5 2.5 2.5-1.12 2.5-2.5-1.12-2.5-2.5-2.5z"
+							fillRule="nonzero"
+						/>
+					</Icon>
+				)}
 			</div>
 			<div className="flex justify-between wrap bg-orange text-white text-xs px-4 py-1">
 				<div className="source">
